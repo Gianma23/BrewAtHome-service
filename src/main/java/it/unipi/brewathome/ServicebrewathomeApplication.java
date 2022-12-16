7509ead0000c171ff39686602bc9dd559e4fa23a
Gianmaria Saggini <gianmaria.saggini@gmail.com>
2022-12-16 12:38:42 +0100

    Added database creation

--- src/main/java/it/unipi/brewathome/ServicebrewathomeApplication.java
+++ src/main/java/it/unipi/brewathome/ServicebrewathomeApplication.java
@@ -1,13 +1,29 @@
 package it.unipi.brewathome;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServicebrewathomeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServicebrewathomeApplication.class, args);
-	}
+    private static final String DB_NAME = "gianmaria_saggini";
+    
+    public static void main(String[] args) {
+            
+            try (Connection co = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root","");){
+
+            Statement st = co.createStatement();
+            st.executeUpdate("CREATE SCHEMA IF NOT EXISTS " + DB_NAME + ";");
+            }
+            catch (SQLException sqle) {
+                sqle.printStackTrace();
+            }
+            
+            SpringApplication.run(ServicebrewathomeApplication.class, args);
+    }
 
 }
